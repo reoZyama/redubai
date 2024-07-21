7ec1d411d9d488b64b3930ed6667da46aea605b8
haruu8 <real.hqru@gmail.com>
2024-07-21 16:52:11 +0900

    test

--- src/app/api/weathers/route.ts
+++ src/app/api/weathers/route.ts
@@ -1,17 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import Cors from 'cors';
+import $axios from '@/libs/axiosConfig';
+import { NextRequest, NextResponse } from 'next/server';
 
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
 });
-import $axios from '@/libs/axiosConfig';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   // Run the middleware
-  await runMiddleware(req, res, cors);
-
   const { searchParams } = new URL(req.url as string);
   const q = searchParams.get('q');
   const url = `https://api.weatherapi.com/v1/current.json?q=${q}&key=${
@@ -29,33 +27,4 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
       { status: 500 },
     );
   }
-}
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function,
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  // Run the middleware
-  await runMiddleware(req, res, cors);
-
-  // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' });
 }
